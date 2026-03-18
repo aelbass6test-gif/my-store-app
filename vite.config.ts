ad53f9ef579adc13ef63b9092f631454accfb5db
aelbass6test-gif <aelbass6test@gmail.com>
2026-03-18 13:25:42 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath, URL } from 'url';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
@@ -10,7 +11,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react(), viteSingleFile()],
+      plugins: [react(), tailwindcss(), viteSingleFile()],
       define: {
         // Define process.env as an empty object first to prevent "process is not defined"
         'process.env': {},
