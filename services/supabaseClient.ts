7ab1e657f716a225d4ce0e63d0dd1b70eea32bf4
aelbass6test-gif <aelbass6test@gmail.com>
2026-05-07 18:34:20 +0300

    Update Supabase URL and key in supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -12,8 +12,8 @@ const getEnvVar = (name: string) => {
     return null;
 };
 
-const supabaseUrl = getEnvVar('VITE_SUPABASE_URL') || 'https://keqmlcqymkohxzcouxfi.supabase.co';
-const supabaseKey = getEnvVar('VITE_SUPABASE_ANON_KEY') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlcW1sY3F5bWtvaHh6Y291eGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1ODU0NzAsImV4cCI6MjA4NjE2MTQ3MH0.OfxqWM9CFCcLj62u5KLWZyiiBhUH-miUu882Cqlwf4I';
+const supabaseUrl = getEnvVar('VITE_SUPABASE_URL') || 'https://vefjtqnoowqfdtivtnym.supabase.co';
+const supabaseKey = getEnvVar('VITE_SUPABASE_ANON_KEY') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZmp0cW5vb3dxZmR0aXZ0bnltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNTI1NDgsImV4cCI6MjA5MzcyODU0OH0.o4zcX5L4Hv9RvJlR4UYZ9kn3lp3v_z2SGZsy3fs7x2k';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseKey, {
